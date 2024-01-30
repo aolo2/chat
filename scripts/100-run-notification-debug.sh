d460cd728b8feb4c45ce1cb6966df1ad96ad944b
Maria Keating <maria@mariakeating.com>
2024-01-30 12:41:30 +0800

    Stage 3 script rework.

--- scripts/100-run-notification-debug.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-LD_LIBRARY_PATH="$(realpath $SCRIPT_DIR/../toolchain/gcc):$LD_LIBRARY_PATH" $SCRIPT_DIR/../build/notification-server-debug "$@"
